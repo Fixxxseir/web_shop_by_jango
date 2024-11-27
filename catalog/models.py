55a510e410ea88be04070f8fd913d36d2aaca4e1
mexic <mexic2284@mail.ru>
2024-11-28 02:03:23 +0300

    added on models.py in app catalog two models: 1) Category 2) Product

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=150, verbose_name="Наименование категории", help_text="Введите наименование категории"
+    )
+    description = models.TextField(
+        blank=True, null=True, verbose_name="Описание категории", help_text="Введите описание категории"
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = [
+            "name",
+        ]
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=150, verbose_name="Наименование продукта", help_text="Введите наименование продукта"
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name="Категория",
+        help_text="Введите категорию продукта",
+        null=True,
+        blank=True,
+        related_name="categories",
+    )
+    description_product = models.TextField(
+        blank=True, null=True, verbose_name="Описание продукта", help_text="Введите описание продукта"
+    )
+    image = models.ImageField(
+        upload_to="products/images",
+        blank=True,
+        null=True,
+        verbose_name="Загрузите изображение продукта",
+        help_text="Загрузите изображение продукта",
+    )
+    purchase_price = models.FloatField(verbose_name="Цена продукта", help_text="Введите цену продукта")
+    created_at = models.DateField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateField(auto_now=True, verbose_name="Дата последнего изменения")
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = [
+            "name",
+            "category",
+            "purchase_price",
+        ]
+
+    def __str__(self):
+        return f"{self.name} {self.category}"
