4d98c95e0b8ef3b693ae8ff2ab64b2aa0bd0152a
mexic <mexic2284@mail.ru>
2024-11-28 18:50:04 +0300

    A selection of the last 5 created products has been added to the main page display controller and output to the console.

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from catalog.models import Product
 
 
 def home(request):
+	latest_products = Product.objects.order_by('-created_at')[:5]
+	print(f'Последние 5 создпоследнийанных продуктов: {latest_products}')
 	return render(request, 'catalog/home.html')
 
 
