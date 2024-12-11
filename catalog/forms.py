0e3641c3ed072c0440d470a0e6c05eb1308f218c
mexic <mexic2284@mail.ru>
2024-12-11 16:10:57 +0300

    category вынесена из класса Meta для текста в выпадающем списке что категория не выбрана

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,14 +1,16 @@
 from django import forms
-from catalog.models import Product
+from catalog.models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Категория не выбрана",
+                                      label='Категория продукта', widget=forms.Select(attrs={"class": "form-control"}))
+
     class Meta:
         model = Product
         fields = ["name", "category", "description", "image", "purchase_price"]
         widgets = {
             "name": forms.TextInput(attrs={"class": "form-control"}),
-            "category": forms.Select(attrs={"class": "form-control"}),
             "description": forms.Textarea(attrs={"class": "form-control"}),
             "image": forms.ClearableFileInput(attrs={"class": "form-control-file"}),
             "purchase_price": forms.NumberInput(attrs={"class": "form-control"}),
