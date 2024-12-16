5cff61281b00b09c0f6693a6481417cc800cf4bf
mexic <mexic2284@mail.ru>
2024-12-16 23:56:10 +0300

    добавлен форма для добавления поста в БД

---
+++ BlogHaven/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from BlogHaven.models import Blog
+
+
+class BlogForm(forms.ModelForm):
+
+	class Meta:
+		model = Blog
+		fields = ["title", "slug", "content", "image", "is_published"]
+		widgets = {
+			"title": forms.TextInput(attrs={"class": "form-control"}),
+			"slug": forms.TextInput(attrs={"class": "form-control"}),
+			"content": forms.Textarea(attrs={"class": "form-control"}),
+			"image": forms.ClearableFileInput(attrs={"class": "form-control-file"}),
+		}
