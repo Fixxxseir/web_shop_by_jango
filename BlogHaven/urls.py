8a6ba6cf20febd0d4eb5a2a5f00b569042f4a0a2
mexic <mexic2284@mail.ru>
2024-12-16 23:54:46 +0300

    добавлены пути

---
+++ BlogHaven/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from BlogHaven.apps import BloghavenConfig
+from BlogHaven.views import BlogListView, BlogCreateView, BlogDetailView, BlogUpdateView, BlogDeleteView
+
+app_name = BloghavenConfig.name
+
+
+urlpatterns = [
+	path("blog/", BlogListView.as_view(), name='blog_list'),
+	path("blog/new/", BlogCreateView.as_view(), name='blog_create'),
+	path("blog/post/<slug:blog_slug>/", BlogDetailView.as_view(), name='blog_detail'),
+	path("blog/update/<slug:slug>/", BlogUpdateView.as_view(), name='blog_update'),
+	path("blog/delete/<slug:slug>/", BlogDeleteView.as_view(), name='blog_delete'),
+]
