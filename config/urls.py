7bdefe03a192a423a9c8461394238c68573f163b
mexic <mexic2284@mail.ru>
2025-01-08 02:22:32 +0300

    добавлены путь в settings django для приложения users

--- config/urls.py
+++ config/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.urls", namespace="catalog")),
     path("", include("BlogHaven.urls", namespace="BlogHaven")),
+    path("users/", include("users.urls", namespace="users"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
