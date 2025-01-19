90acd11ef0490ed0c6c8ee0c60e4bae5a30021d7
mexic <mexic2284@mail.ru>
2025-01-19 20:52:38 +0300

    добавлено кеширование пути с одним продуктом добавлен путь для продуктов по категориям

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from catalog.apps import CatalogConfig
 from catalog.views import (
-    ProductHomeView,
+    CategoryProductListView,
     ProductContactsView,
-    ProductListView,
-    ProductDetailView,
     ProductCreateView,
+    ProductDeleteView,
+    ProductDetailView,
+    ProductHomeView,
+    ProductListView,
     ProductUpdateView,
-    ProductDeleteView, StatusProductView,
+    StatusProductView,
 )
 
 app_name = CatalogConfig.name
@@ -16,9 +20,10 @@ urlpatterns = [
     path("", ProductHomeView.as_view(), name="home"),
     path("catalog/contacts/", ProductContactsView.as_view(), name="contacts"),
     path("catalog/product_list/", ProductListView.as_view(), name="product_list"),
-    path("catalog/product/<slug:product_slug>/", ProductDetailView.as_view(), name="product_detail"),
+    path("catalog/product/<slug:product_slug>/", cache_page(60)(ProductDetailView.as_view()), name="product_detail"),
     path("catalog/add_product/", ProductCreateView.as_view(), name="add_product"),
     path("catalog/update/<slug:update_slug>", ProductUpdateView.as_view(), name="product_update"),
     path("catalog/delete/<slug:slug>", ProductDeleteView.as_view(), name="product_delete"),
     path("catalog/status/<slug:product_slug>", StatusProductView.as_view(), name="status_product"),
+    path("catalog/category_product/<int:category_id>/", CategoryProductListView.as_view(), name="category_product"),
 ]
